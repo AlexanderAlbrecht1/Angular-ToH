034501e5e83f5cd5439a43b455a7e79538cf35b4
Alexander <alexander.albrecht1@gmail.com>
2025-07-16 13:22:36 +0200

    Display hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
-import { UpperCasePipe } from '@angular/common';
+import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
+
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm',
   };
+
+  selectedHero?:Hero;
+  onSelect(hero:Hero):void {
+    this.selectedHero = hero;
+  }
 }
